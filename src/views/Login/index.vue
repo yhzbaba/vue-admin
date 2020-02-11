64cb182557453a55759bb7b49eca84d8080b99eb
yhzbaba <2679122209@qq.com>
2020-02-11 23:24:42 +0800

    vue基本操作和一个高光功能

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,22 +1,66 @@
 <template>
   <div id="login">
-    这是登陆页面
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li
+          :class="{ current: item.current }"
+          v-for="item in menuTab"
+          :key="item.id"
+          @click="toggleMenu(item)"
+        >
+          {{ item.txt }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: "login"
+  name: "login",
+  data() {
+    return {
+      menuTab: [
+        { txt: "登录", current: true },
+        { txt: "注册", current: false }
+      ],
+      isActive: false
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    toggleMenu(data) {
+      this.menuTab.forEach(elem => {
+        elem.current = false;
+      });
+      //高光
+      data.current = true;
+    }
+  }
 };
 </script>
-<style scoped>
+<style lang="scss" scoped>
 #login {
   height: 100vh;
-  background-color: #344a5f;
+  background-color: #517494;
+}
+.login-wrap {
+  width: 330px;
+  margin: auto;
+}
+.menu-tab {
+  text-align: center;
+  li {
+    display: inline-block;
+    width: 88px;
+    line-height: 36px;
+    font-size: 14px;
+    color: #fff;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+  .current {
+    background-color: rgba($color: #000000, $alpha: 1);
+  }
 }
 </style>
